fbbb60b2530e0ab2dfa1cf78093fded8045eb667
xsjinn <137873332+xsjinn@users.noreply.github.com>
2024-08-20 06:02:04 +0100

    Add files via upload

---
+++ webapp/vsearch.py
@@ -0,0 +1,10 @@
+
+
+def search4vowels(phrase: str) -> set:
+ """Returns the set of vowels found in 'phrase'."""
+ return set('aeiou').intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+ """Returns the set of 'letters' found in 'phrase'."""
+ return set(letters).intersection(set(phrase))
